1df9f0c5a14e6b2900e17b1758415e8705fed687
mksnm <miju.kasanami@gmail.com>
2022-01-16 19:34:44 +0900

    built

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -13,7 +13,7 @@ Vue.use(Router)
 
 export const routerOptions = {
   mode: 'history',
-  base: '/mksnm.github.io/',
+  base: '/',
   linkActiveClass: 'nuxt-link-active',
   linkExactActiveClass: 'nuxt-link-exact-active',
   scrollBehavior,
